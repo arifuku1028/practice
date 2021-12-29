4e40fb08dea1a7571b485536c9ec7c0c1c8aec57
Hiroshi Arifuku <zm73aym@gmail.com>
2021-12-29 11:19:23 +0000

    Associate user with tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,7 +5,10 @@ class TweetsController < ApplicationController
   end
 
   def create
-    
+    @tweet = Tweet.new(tweet_params)
+    @tweet.user_id = current_user.id
+    @tweet.save
+    redirect_to tweets_path
   end
   
   def index
@@ -14,6 +17,11 @@ class TweetsController < ApplicationController
 
   def show
     @tweet = Tweet.find(params[:id])
+  end
+  
+  private
+  def tweet_params
+    params.require(:tweet).permit(:body)
   end
 
 end
