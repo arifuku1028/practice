11a8f1ef4f73a1a99df374eb7997b21c6d4b792e
Hiroshi Arifuku <zm73aym@gmail.com>
2022-01-10 05:24:12 +0000

    Add Image-posting Function

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -8,6 +8,14 @@ class TweetsController < ApplicationController
   def create
     @tweet = Tweet.new(tweet_params)
     @tweet.user_id = current_user.id
+    image_file = tweet_params[:image]
+    tweet_name = image_file.original_filename
+    @tweet.image = tweet_name
+    if image_file.present?
+      image_dir = Rails.root.join("public", "images")
+      image_path = image_dir + tweet_name
+      File.binwrite(image_path, image_file.read)
+    end
     @tweet.save
     redirect_to tweets_path
   end
@@ -20,11 +28,15 @@ class TweetsController < ApplicationController
     @tweet = Tweet.find(params[:id])
     @user = @tweet.user
     @comments = @tweet.comments
+    if @tweet.image
+      image_dir = "/images/"
+      @image_path = image_dir + @tweet.image
+    end
   end
   
   private
   def tweet_params
-    params.require(:tweet).permit(:body)
+    params.require(:tweet).permit(:body, :image)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_01_111050) do
+ActiveRecord::Schema.define(version: 2022_01_10_003417) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "user_id"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2022_01_01_111050) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
