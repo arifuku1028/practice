6bf77a89492da06a8f2c12cfaf081407ce631879
Hiroshi Arifuku <zm73aym@gmail.com>
2022-01-10 13:12:31 +0000

    Debug program errors

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -8,23 +8,6 @@ class TweetsController < ApplicationController
   def create
     @tweet = Tweet.new(tweet_params)
     @tweet.user_id = current_user.id
-    #image_file = tweet_params[:image]
-    #image_name = image_file.original_filename
-    #if image_file.present?
-      #image_dir = "images/"
-      ##image_dir = Rails.root.join("public", "images")
-      #image_path = image_dir + image_name
-      
-      #bucket_name = "practice-s3-20220110"
-      #s3 = Aws::S3::Resource.new
-      #bucket = s3.bucket(bucket_name)
-      #if bucket.exists?
-        #bucket.put_object(acl: "public", key: image_path, body: image_file.read) 
-        #@tweet.image = image_path
-        ##@tweet.image = "/" + image_path
-      #end
-      ##File.binwrite(image_path, image_file.read)
-    #end
     @tweet.save
     redirect_to tweets_path
   end
